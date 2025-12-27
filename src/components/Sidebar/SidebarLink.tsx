c7b39f7d82508a3896aa90341c8c6fef613652e6
alteropt <vladqwer035@gmail.com>
2025-12-27 16:59:36 +0300

    Added video to single show page

--- src/components/Sidebar/SidebarLink.tsx
+++ src/components/Sidebar/SidebarLink.tsx
@@ -6,7 +6,7 @@ const SidebarLink = ({ link }: { link: sidebarLink }) => {
 		<NavLink
 			to={link.link}
 			className={({ isActive }) =>
-				'relative text-(--secondary-color) pl-8 py-3 inline-flex w-fit items-center gap-2 transition-colors duration-300 text-sm before:content-[""] before:absolute before:w-1 before:top-1/2 before:rounded-r-2xl before:translate-y-[-50%] before:bg-(--accent-color) before:left-0 before:transition-all before:duration-300 before:h-0 hover:before:h-full hover:bg-linear-to-r hover:from-[#9c9d9d20] hover:to-(--background-color) hover:text-(--text-color) group' +
+				'relative text-(--secondary-color) pl-8 py-3 inline-flex w-fit items-center gap-2 transition-colors duration-300 text-sm before:content-[""] before:absolute before:w-1 before:top-1/2 before:rounded-r-2xl before:translate-y-[-50%] before:bg-(--accent-color) before:left-0 before:transition-all before:duration-300 before:h-0 hover:before:h-full hover:bg-linear-to-r hover:from-[#9c9d9d20] hover:to-(--background-color) hover:text-(--accent-color) group' +
 				(isActive
 					? 'before:h-full bg-linear-to-r from-[#9c9d9d20] to-(--background-color) text-(--accent-color)!'
 					: '')
--- src/pages/ShowPage.tsx
+++ src/pages/ShowPage.tsx
@@ -2,15 +2,38 @@ import { useParams } from 'react-router'
 import { useShow } from '../hooks/useShows'
 
 const ShowPage = () => {
-	const { id } = useParams<{ id: string }>()
+	const { id } = useParams()
+	const { data: show, isLoading, error } = useShow(Number(id))
 
-	const { data: show } = useShow(Number(id))
+	if (!id) return <div>Invalid show id</div>
+	if (isLoading) return <div>Loading...</div>
+	if (error) return <div>Error: {error.message}</div>
+	if (!show) return <div>Show is Not Found</div>
 
 	return (
-		<div>
-			<h1 className='text-3xl font-bold'>
-				Show <img src={show?.poster} alt='' />
-			</h1>
+		<div className='relative h-screen w-full overflow-hidden'>
+			<video
+				className='absolute inset-0 h-full w-full object-contain pointer-events-none filter blur-xs'
+				autoPlay
+				muted
+				loop
+				playsInline
+				preload='auto'
+			>
+				<source src={show.trailerUrl} type='video/mp4' />
+			</video>
+
+			<div className='absolute inset-0 bg-black/50' />
+
+			<div className='relative z-10 text-white p-10'>
+				<h1 className='text-4xl font-bold'>{show.title}</h1>
+				<p className='mt-4 max-w-xl'>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam,
+					distinctio fuga! Dolores iste tempore ducimus ipsam, quam eos
+					voluptates quo dolorem pariatur tempora voluptate eveniet et maxime
+					libero, illo eum?
+				</p>
+			</div>
 		</div>
 	)
 }
